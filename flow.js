3f6634ccaa50cd0bc022ba23b8aa000c5652e122
Dan Andrei <andreidani96@gmail.com>
2017-02-15 16:33:47 +0200

    wip refactor

--- flow.js
+++ flow.js
@@ -1,94 +1,62 @@
-"use strict";
+'use strict';
 
-const Parser = require("./lib/Parser");
-const Manipulation = require("./lib/Manipulation");
-const Interaction = require("./lib/Interaction");
-const Context = require("./lib/context");
-const Navigation = require("./lib/navigation");
+const VIS = require('./index');
 
-const visInstances = {};
-function getVisualization (name, config) {
+exports.init = (scope, state, args, data, next) => {
 
-    if (visInstances[name]) {
-        return visInstance[name];
-    } else {
-        visInstance[name] = Visualizer(config);
-    }
-}
-
-exports.getVis = function (scope, state, args, data, next) {
-
-    data.vis = getVisualization(data.name || args.name, args.vis);
-
-    if (!(state.config.view = document.querySelector(args.view))) {
-        return next(new Error("Flow-visualizer: DOM target not found."));
+    if (!args.parse) {
+        return next(new Error('Flow-visualizer.init: No parse config found.'));
     }
 
-    state.index = {
-        nodes: {},
-        edges: {}
-    };
+    if (!args.events) {
+        return next(new Error('Flow-visualizer.init: No event interface config found.'));
+    }
 
-    state.nodes = new vis.DataSet([]);
-    state.edges = new vis.DataSet([]); 
+    if (!args.view || !document.querySelector(args.view)) {
+        return next(new Error('Flow-visualizer: DOM data.node not found.'));
+    }
 
-    state.network = new vis.Network(state.config.view, {
-        nodes: state.nodes,
-        edges: state.edges
-    }, state.visConfig);
+    state.VIS = VIS(args);
 
-    // TODO set selected nodes and edges on state
-    Interaction.init(scope, state);
-    //Context.init(scope, state);
+    return next(null, data);
+};
 
-    next(null, data);
+exports.add = (scope, state, args, data, next) => {
+
+    return next(null, data);
+};
+
+exports.remove = (scope, state, args, data, next) => {
+
+    return next(null, data);
+};
+
+exports.reset = (scope, state, args, data, next) => {
+
+    return next(null, data);
+};
+
+exports.parse = (scope, state, args, data, next) => {
+
+    return next(null, data);
+};
+
+exports.getSelectedNode = (scope, state, args, data, next) => {
+
+    return next(null, data);
+};
+
+exports.zoom = (scope, state, args, data, next) => {
+
+    return next(null, data);
 };
 
 exports.focus = (scope, state, args, data, next) => {
 
-    if (!true) {
-        return new Error();
-    }
-
-    return Stream.Transform({
-        transform: (chunk, next) => {
-            if (!data.vis || !data.vis.focus) {
-                return;
-            }
-
-            data.vis.focus();
-        }
-    })
+    return next(null, data);
 };
 
-// TODO buttons
-exports.context = function (scope, state, args, data, next) {
+exports.select = (scope, state, args, data, next) => {
 
-    if (!data.node) {
-        return next(new Error("Flow-visualizer.context: No node provided."));
-        //return next(null, data);
-    }
-
-    Context.toggle(state, data.node);
-
-    next(null, data);
+    return next(null, data);
 };
-
-// TODO remove this method, when selected nodes and edges are saved on the state
-// ..navigation uses this function
-exports.getSelectedNode = function (scope, state, args, data, next) {
-
-    let selectedNodes = state.network.getSelectedNodes();
-    data.node = (selectedNodes && selectedNodes.length) ? selectedNodes[0] : null;
-
-    next(null, data);
-};
-
-/* Export Navigation methods */
-exports.navigateSelectedNode = Navigation.navigateSelectedNode;
-
-/* Export Interaction methods */
-// ..animations and user events
-exports.focus = Interaction.focus;
-exports.expandCollapse = Interaction.expandCollapse;
-exports.changeFocusZoom = Interaction.changeFocusZoom;
--- index.js
+++ index.js
@@ -1,81 +1,87 @@
-"use strict"
+'use strict'
 
-const Events = require("events");
-const Parser = require("./lib/Parser");
-const Interaction = require("./lib/Interaction");
-const Manipulation = require("./lib/Manipulation");
-const Visualization = require("./lib/visulaization");
+// Dependencies
+const Events = require('events');
+const Parser = require('./lib/Parser');
+const Interaction = require('./lib/Interaction');
+const Manipulation = require('./lib/Manipulation');
+const Visualization = require('./lib/Visualization');
+const Navigation = require('./lib/Navigation');
 
-function verifyConfig (config) {
+function buildOptions (config) {
 
     // create visjs only config object
-    state.config = {};
-    state.visConfig = {
-        nodes: {
-            shape: "dot",
-            scaling: { min: 20,max: 30,
-                label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
+    let options = {
+        vis: {
+            nodes: {
+                shape: 'dot',
+                scaling: { min: 20,max: 30,
+                    label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
+                },
+                font: {size: 14, face: 'Helvetica Neue, Helvetica, Arial'}
             },
-            font: {size: 14, face: "Helvetica Neue, Helvetica, Arial"}
+            interaction: {
+                hover: true,
+                hoverConnectedEdges: false,
+                selectConnectedEdges: true
+            }
         },
-        interaction: {
-            hover: true,
-            hoverConnectedEdges: false,
-            selectConnectedEdges: true
-        }
+        config: {}
     };
 
-    Object.keys(args).forEach((key) => {
+    Object.keys(config).forEach((key) => {
         switch (key) {
-            case "events":
-            case "parse":
-            case "view":
-            case "buttons":
-            case "colors":
-                state.config[key] = args[key];
+            case 'events':
+            case 'parse':
+            case 'view':
+            case 'buttons':
+            case 'colors':
+                options.config[key] = config[key];
                 return;
-            case "nodes":
-            case "edges":
-                state.visConfig[key] = {};
-                state.config[key] = {};
-                Object.keys(args[key]).forEach((subKey) => {
+            case 'nodes':
+            case 'edges':
+                options.vis[key] = {};
+                options.config[key] = {};
+                Object.keys(config[key]).forEach((subKey) => {
                     switch (subKey) {
-                        case "events":
-                        case "types":
-                        case "expand":
-                        case "open":
-                            state.config[key][subKey] = args[key][subKey];
+                        case 'events':
+                        case 'types':
+                        case 'expand':
+                        case 'open':
+                            options.config[key][subKey] = config[key][subKey];
                             return;
                     }
-                    state.visConfig[key][subKey] = args[key][subKey];
+                    options.vis[key][subKey] = config[key][subKey];
                 });
                 return;
         }
 
-        state.visConfig[key] = args[key];
+        options.vis[key] = config[key];
     });
-    return
+
+    return options;
 }
 
 module.exports = (config) => {
+    let VIS = new Events();
 
-    const VIS = new Events();
-    const interaction = Interaction(VIS, config);
-    const manipulation = Manipulation(VIS, config);
-    const parser = Parser(VIS, config);
+    // parse the config
+    let options = buildOptions(config);
 
-    // parse RDF strea-m
-    VIS.parse: () => {},
+    // init all modules
+    let visualization = new Visualization({
+        view: options.config.view,
+        vis: options.vis
+    });
+    let parser = new Parser({
+        config: options.config
+    });
+    let navigation = new Navigation();
+    let interaction = new Interaction();
+    let manipulation = new Manipulation();
 
-    // interaction
-    VIS.zoom: () => {},
-    VIS.focus: () => {},
-    VIS.select: () => {},
-
-    // manipulation
-    VIS.add: () => {},
-    VIS.remove: () => {},
-    VIS.reset: () => {}
+    // parse rdf data
+    VIS.parse = parser.parse;
 
     return Object.freeze(VIS);
 };
