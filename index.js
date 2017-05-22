5194b042e64569f448b07b4f20dff61557623f54
Dan Andrei <andreidani96@gmail.com>
2017-05-22 15:51:03 +0300

    Parser, add and remove refactor.

--- index.js
+++ index.js
@@ -1,100 +1,88 @@
 'use strict'
 
 // Dependencies
+const Vis = require('vis');
 const Events = require('events');
 const Parser = require('./lib/Parser');
-const Interaction = require('./lib/Interaction');
 const Manipulation = require('./lib/Manipulation');
-const Visualization = require('./lib/Visualization');
-const Navigation = require('./lib/Navigation');
 
-function buildOptions (config) {
+// const values
+const CONFIG_DEFAULTS = {
+    container: '.graph',
+    vis: {},
+    colors: {},
+    buttons: {}
+};
 
-    // create visjs only config object
-    let options = {
-        vis: {
-            nodes: {
-                shape: 'dot',
-                scaling: { min: 20,max: 30,
-                    label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
-                },
-                font: {size: 14, face: 'Helvetica Neue, Helvetica, Arial'}
-            },
-            interaction: {
-                hover: true,
-                hoverConnectedEdges: false,
-                selectConnectedEdges: true
-            }
-        },
-        config: {}
+module.exports = (config) => {
+    let self = new Events();
+
+    // check config integrity
+    checkConfig(config);
+    self.config = config;
+
+    // initialize graph
+    self.nodes = new Vis.DataSet([]);
+    self.edges = new Vis.DataSet([]);
+
+    self.index = {
+        nodes: {},
+        edges: {}
     };
 
-    Object.keys(config).forEach((key) => {
-        switch (key) {
-            case 'events':
-            case 'parse':
-            case 'view':
-            case 'buttons':
-            case 'colors':
-                options.config[key] = config[key];
-                return;
-            case 'nodes':
-            case 'edges':
-                options.vis[key] = {};
-                options.config[key] = {};
-                Object.keys(config[key]).forEach((subKey) => {
-                    switch (subKey) {
-                        case 'events':
-                        case 'types':
-                        case 'expand':
-                        case 'open':
-                            options.config[key][subKey] = config[key][subKey];
-                            return;
-                    }
-                    options.vis[key][subKey] = config[key][subKey];
-                });
-                return;
-        }
+    self.container = document.querySelector(self.config.container);
+    if (!self.container) {
+        throw new Error('Flow-visualizer: DOM graph ref not found.');
+    }
 
-        options.vis[key] = config[key];
-    });
+    self.network = new Vis.Network(self.container, {
+        nodes: self.nodes,
+        edges: self.edges
+    }, self.config.vis);
 
-    return options;
-}
 
-function VIS (config) {
-    // parse the config
-    let options = buildOptions(config);
-    this.options = options;
+    // export visualization methods
+    self.add = Manipulation.add;
+    self.remove = Manipulation.remove;
+    self.reset = Manipulation.reset;
+    self.parse = Parser.parse.bind(self);
+    self.getSelectedNode = () => {
+        let selectedNodes = self.network.getSelectedNodes();
+        let node = (selectedNodes && selectedNodes.length) ? selectedNodes[0] : null;
 
-    // init all modules
-    this.visualization = new Visualization({
-        view: options.config.view,
-        vis: options.vis
-    });
-    this.parser = new Parser({
-        config: options.config
-    });
-    this.navigation = new Navigation(this.visualization);
-    this.interaction = new Interaction(this.visualization, options.config);
-    this.manipulation = new Manipulation(this.visualization);
-
-    // parse rdf data
-    this.parse = (triples, startNode, onlyParse) => {
-
-        if (!startNode) {
-            startNode = { id: '' };
-        }
-
-        let pos = {};
-        pos.x = startNode.x || 0;
-        pos.y = startNode.y || 0;
-        pos.l = this.visualization.network.getBoundingBox(startNode.id) || {top: 0, left: 0, bottom: 0, right: 0};
-        pos.l = Math.sqrt(Math.pow(pos.l.top - pos.l.bottom, 2) + Math.pow(pos.l.right - pos.l.left, 2));
-        pos.parent = startNode.parent ? this.visualization.network.getPositions(startNode.parent)[startNode.parent] : {x: 0, y: 0};
-
-        return this.parser.parse(triples, startNode, pos, onlyParse ? null : this.visualization.index);
+        return node;
     };
-}
 
-module.exports = VIS;
+    return self;
+};
+
+/* Private functions */
+
+/* checkConfig
+ *
+ * Check config object integrity and add defaults
+ *
+ */
+function checkConfig (config) {
+    config = config || {};
+
+    Object.keys(CONFIG_DEFAULTS).forEach(key => {
+        config[key] = config[key] || CONFIG_DEFAULTS[key];
+    });
+
+    if (!config.parse) {
+        throw new Error('Flow-visualizer: Parse config is missing.');
+    }
+
+    if (!config.parse.nodes) {
+        throw new Error('Flow-visualizer: Parse nodes config is missing.');
+    }
+
+    if (!config.parse.edges) {
+        throw new Error('Flow-visualizer: Parse edges config is missing.');
+    }
+
+    if (!config.parse.triples) {
+        throw new Error('Flow-visualizer: Parse triples config is missing.');
+    }
+}
